fb3720e44d5604f89b2b9cd08af02308c864f67e
Sourin <iamsourinsarkar@gmail.com>
2023-12-30 19:30:56 +0530

    Added Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 const App: React.FC = () => {
   return (
-    <div></div>
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
   );
-}
+};
 
-export default App
+export default App;
