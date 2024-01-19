729d5252020a375210b0ff6a1247d7b9085db162
Sourin <iamsourinsarkar@gmail.com>
2024-01-19 23:42:38 +0530

    Completed the design of the password component

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'iconColor': "#2C3548",
+        'iconBgColor': "#F5F5F5",
+        'primaryFontColor': "#27282C",
+        'primeTextColor': "#5D6576",
+      },
+      boxShadow: {
+        'sh_Light': "0px 1.5px 4px 0px rgba(0, 0, 0, 0.15)",
+      }
+    },
   },
   plugins: [],
 }
